c77ccf14b7beb9133e8f54fe89e741cc35657ff2
PegasusTechnology <pegasustech@outlook.com>
2015-02-05 01:57:20 +0530

    External Logins integrated

--- RIO/App_Start/AuthConfig.cs
+++ RIO/App_Start/AuthConfig.cs
@@ -14,19 +14,23 @@ namespace RIO
             // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
             // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
 
-            //OAuthWebSecurity.RegisterMicrosoftClient(
-            //    clientId: "",
-            //    clientSecret: "");
+            //P@$$word1 - James Bonda - iam_invincible@outlook.com
+            OAuthWebSecurity.RegisterMicrosoftClient(
+                clientId: "0000000040140814",
+                clientSecret: "QkX9bqz0h1kE-wB5aDluDVoWp6UEXh9T");
 
             //OAuthWebSecurity.RegisterTwitterClient(
             //    consumerKey: "",
             //    consumerSecret: "");
 
-            //OAuthWebSecurity.RegisterFacebookClient(
-            //    appId: "",
-            //    appSecret: "");
+            //P@$$word1 - James Bonda
+            OAuthWebSecurity.RegisterFacebookClient(
+                appId: "541694889266197",
+                appSecret: "35fd71113ae77e5329ecf8001c7f12c1");
 
             OAuthWebSecurity.RegisterGoogleClient();
+
+            OAuthWebSecurity.RegisterYahooClient();
         }
     }
 }
